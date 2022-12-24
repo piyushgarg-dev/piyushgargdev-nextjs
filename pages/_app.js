9e43d775d29bc9a8ca952b38ffdcd87a63f9dfe0
Piyush Garg <piyushgarg.dev@gmail.com>
2022-12-24 23:07:53 +0530

    Added Google Analytics Script

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,25 @@ import "remixicon/fonts/remixicon.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Layout from "../components/Layout/Layout";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      {process.env.NODE_ENV === "production" && (
+        <>
+          <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=G-0GK7ZH57SK"
+          ></Script>
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-0GK7ZH57SK');
+        `}
+          </Script>
+        </>
+      )}
       <Component {...pageProps} />;
     </Layout>
   );
