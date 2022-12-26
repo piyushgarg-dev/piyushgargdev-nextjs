b92c36c0934755dcc80506107b1bb91f2d57fb71
Piyush Garg <piyushgarg.dev@gmail.com>
2022-12-26 17:08:49 +0530

    Project page is now dynamic

---
+++ components/data/projects.js
@@ -0,0 +1,14 @@
+const projects = [
+  {
+    title: "Tic Tac Toe - Javascript",
+    slug: "tic-tac-toe",
+    tags: ["javascript"],
+    sourceCodeDownloadUrl: "/files/code/tic-tac-toe.zip",
+    youtubeEmbedUrl: "https://www.youtube.com/embed/lKe57l8sttw",
+    hostedWebUrl: "https://ymiic3.csb.app/",
+    codeIframeUrl:
+      "https://codesandbox.io/embed/frosty-lake-ymiic3?fontsize=14&hidenavigation=1&theme=dark&view=editor",
+  },
+];
+
+export default projects;
---
+++ pages/projects/[slug].jsx
@@ -0,0 +1,128 @@
+import Head from "next/head";
+import { Container, Row, Col } from "reactstrap";
+import Browser, { Chrome } from "react-browser-ui";
+import { isMobile } from "react-device-detect";
+
+import projects from "../../components/data/projects";
+
+const { Tab } = Chrome;
+
+import { useSession, signIn } from "next-auth/react";
+
+const Page = ({ project }) => {
+  const { status } = useSession();
+
+  if (status === "loading")
+    return (
+      <Container>
+        <h3>Loading</h3>
+      </Container>
+    );
+
+  if (status === "unauthenticated") {
+    signIn();
+    return;
+  }
+
+  return (
+    <Container className="my-5">
+      <Head>
+        <title>{project.title}</title>
+      </Head>
+      <h3>{project.title}</h3>
+      <Container className="my-3" />
+
+      <Row>
+        {project?.youtubeEmbedUrl && (
+          <Col sm="12" lg="6" md="6">
+            <iframe
+              width="100%"
+              height="405"
+              src={project.youtubeEmbedUrl}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </Col>
+        )}
+        {project.hostedWebUrl && (
+          <Col hidden={isMobile} sm="12" lg="6" md="6">
+            <div
+              style={{
+                background: "#fff",
+                borderRadius: "10px",
+                height: "100%",
+              }}
+            >
+              <Browser activeTabKey={"main"} type={"chrome"}>
+                <Tab key={"main"} title={"Tic Tac Toe"}>
+                  <div style={{ height: "100%" }}>
+                    <iframe
+                      width="100%"
+                      height="100%"
+                      src={project.hostedWebUrl}
+                    />
+                  </div>
+                </Tab>
+              </Browser>
+            </div>
+          </Col>
+        )}
+      </Row>
+
+      {project?.codeIframeUrl && (
+        <Row>
+          <Col lg="12" sm="12" md="12">
+            <iframe
+              src={project.codeIframeUrl}
+              style={{
+                width: "100%",
+                height: "600px",
+                border: "1px solid",
+                margin: "40px 0px",
+                borderRadius: "4px",
+                overflow: "hidden",
+              }}
+              title="frosty-lake-ymiic3"
+              allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
+              sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
+            ></iframe>
+          </Col>
+        </Row>
+      )}
+      {project?.sourceCodeDownloadUrl && (
+        <Row className="text-center">
+          <h3>
+            <a
+              style={{ color: "#fff", textDecoration: "none" }}
+              href={project.sourceCodeDownloadUrl}
+            >
+              <i className="ri-download-line"></i> Download Code
+            </a>
+          </h3>
+        </Row>
+      )}
+    </Container>
+  );
+};
+
+export const getStaticPaths = () => {
+  return {
+    paths: projects.map((project) => ({ params: { slug: project.slug } })),
+    fallback: false,
+  };
+};
+
+export const getStaticProps = (context) => {
+  const slug = context.params.slug;
+  const project = projects.find((e) => e.slug === slug);
+
+  return {
+    props: {
+      project,
+    },
+  };
+};
+
+export default Page;
