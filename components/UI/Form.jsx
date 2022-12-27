2794f46c03ffd2ff29506af5fa412eb054e5ec63
Piyush Garg <piyushgarg.dev@gmail.com>
2022-12-27 22:41:38 +0530

    Added Google Sheet Integration

--- components/UI/Form.jsx
+++ components/UI/Form.jsx
@@ -1,27 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSession } from "next-auth/react";
 import classes from "../../styles/form.module.css";
+import { MagnifyingGlass } from "react-loader-spinner";
 
 const Form = () => {
-  const submitHandler = () => {
-    e.preventDefault();
-  };
+  const { data, status } = useSession();
+
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [message, setMessage] = useState();
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleFormSubmit = React.useCallback(
+    async (e) => {
+      e.preventDefault();
+      await fetch("/api/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          message,
+          isAuthenticated: status === "authenticated" ? "Yes" : "No",
+          authenticatedUserName: data?.user?.name,
+          authenticatedUserEmail: data?.user?.email,
+          authenticatedUserImageUrl: data?.user?.image,
+        }),
+      });
+      setIsSubmitted(true);
+    },
+    [data, email, message, name, status]
+  );
+
+  React.useEffect(() => {
+    if (data && data.user) {
+      setName(data.user.name);
+      setEmail(data.user.email);
+    }
+  }, [data]);
+
+  if (status === "loading") return <MagnifyingGlass />;
+
+  if (isSubmitted) {
+    return (
+      <div
+        style={{
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <h3>Thanks for submission!</h3>
+      </div>
+    );
+  }
+
   return (
-    <form className={`${classes.form}`} onSubmit={submitHandler}>
-      <div className="text-center">
-        <p>
-          <i className="ri-alarm-warning-line"> </i>I was feeling a bit lazy and
-          I haven{"'"}t connected this form to my backend{" "}
-          <i className="ri-alarm-warning-line"></i>
-        </p>
+    <form className={`${classes.form}`} onSubmit={handleFormSubmit}>
+      <div className={`${classes.form__group}`}>
+        <input
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          name="name"
+          type="text"
+          placeholder="Your Name"
+          required
+        />
       </div>
       <div className={`${classes.form__group}`}>
-        <input type="text" placeholder="Your Name" required />
+        <input
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          name="email"
+          type="email"
+          placeholder="Email Address"
+          required
+        />
       </div>
       <div className={`${classes.form__group}`}>
-        <input type="email" placeholder="Email Address" required />
-      </div>
-      <div className={`${classes.form__group}`}>
-        <textarea type="text" rows={5} placeholder="Message" required />
+        <textarea
+          onChange={(e) => setMessage(e.target.value)}
+          value={message}
+          name="message"
+          type="text"
+          rows={5}
+          placeholder="Message"
+          required
+        />
       </div>
 
       <button className="primary__btn" type="submit">
--- pages/api/hello.js
+++
@@ -1,7 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default async function handler(req, res) {
-  res.status(200).json({ success: "success" });
-
-  client.close();
-}
