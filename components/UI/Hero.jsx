060b0e5ec6dd65b574fef68b51c7d37314b5ce12
Piyush Garg <piyushgarg.dev@gmail.com>
2022-12-28 18:00:10 +0530

    Reduce Content

--- components/UI/Hero.jsx
+++ components/UI/Hero.jsx
@@ -15,13 +15,11 @@ const Hero = () => {
               <SectionSubtitle subtitle="Hello" />
               <h2 className="mt-3 mb-3">I&apos;m Piyush Garg</h2>
               <h5 className="mb-4">Fullstack Developer & Instructor</h5>
-              <p style={{ maxHeight: "250px" }} id="about-me">
+              <p id="about-me">
                 Hi there! My name is Piyush Garg and I&rsquo;m a software
                 engineer with over 5 years of experience in the industry. I love
                 all things tech and coding, and on my channel, I share my
-                knowledge and experience with others. Whether you&rsquo;re a
-                beginner looking to learn the basics or an experienced developer
-                looking to expand your skills, I&rsquo;ve got something for you.
+                knowledge and experience with others.
               </p>
             </div>
           </Col>
