3a01b8b7bf7e808faf3ed9f9853839a3fcb98e0f
Piyush Garg <piyushgarg.dev@gmail.com>
2022-12-28 18:16:57 +0530

    Lazy Import

--- components/UI/Testimonial.jsx
+++ components/UI/Testimonial.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
-import Image from "next/image";
 import SectionSubtitle from "./SectionSubtitle";
-import network from "../../public/images/Connected world.png";
 import Slider from "react-slick";
-import classes from "../../styles/testimonial.module.css";
-import Link from "next/link";
+import dynamic from "next/dynamic";
+import { MagnifyingGlass } from "react-loader-spinner";
+
+const TestimonialItem = dynamic(() => import("./TestimonialItem"), {
+  loading: <MagnifyingGlass />,
+});
 
 const Testimonial = ({ feedbacks = [] }) => {
   const settings = {
@@ -47,44 +49,7 @@ const Testimonial = ({ feedbacks = [] }) => {
           <Slider {...settings}>
             {feedbacks.map((feedBack) => (
               <Col key={feedBack.name} lg="4" md="4" sm="12">
-                <div className={`${classes.testimonial__item}`}>
-                  <div className={`${classes.testimonial__client}`}>
-                    <Image
-                      alt={feedBack.name}
-                      src={
-                        feedBack.userImage ??
-                        "https://wsrv.nl/?url=https%3A%2F%2Fcodedamn.com%2Fassets%2Fimages%2Favatar.png&w=80&fit=cover&h=80&q=70&output=webp"
-                      }
-                      width="50"
-                      height="50"
-                      className=" rounded-2"
-                    />
-
-                    <div>
-                      <h6>{feedBack.name}</h6>
-
-                      <h6>
-                        <Link
-                          aria-label={feedBack.courseName}
-                          target="_blank"
-                          href={feedBack.navigateToUrl}
-                        >
-                          {feedBack.courseName}
-                        </Link>
-                      </h6>
-                    </div>
-                  </div>
-                  {Array(5)
-                    .fill(0)
-                    .map((_, index) => (
-                      <i
-                        key={index}
-                        style={{ color: "#FFD700" }}
-                        className="ri-star-fill"
-                      />
-                    ))}
-                  <p style={{ fontSize: "20px" }}>{feedBack.content}</p>
-                </div>
+                <TestimonialItem feedBack={feedBack} />
               </Col>
             ))}
           </Slider>
---
+++ components/UI/TestimonialItem.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import classes from "../../styles/testimonial.module.css";
+
+const TestimonialItem = ({ feedBack }) => (
+  <div className={`${classes.testimonial__item}`}>
+    <div className={`${classes.testimonial__client}`}>
+      <Image
+        onLoad={(e) => console.log("Feedback Loaded", feedBack.name)}
+        alt={feedBack.name}
+        src={
+          feedBack.userImage ??
+          "https://wsrv.nl/?url=https%3A%2F%2Fcodedamn.com%2Fassets%2Fimages%2Favatar.png&w=80&fit=cover&h=80&q=70&output=webp"
+        }
+        width="50"
+        height="50"
+        className=" rounded-2"
+      />
+
+      <div>
+        <h6>{feedBack.name}</h6>
+
+        <h6>
+          <Link
+            aria-label={feedBack.courseName}
+            target="_blank"
+            href={feedBack.navigateToUrl}
+          >
+            {feedBack.courseName}
+          </Link>
+        </h6>
+      </div>
+    </div>
+    {Array(5)
+      .fill(0)
+      .map((_, index) => (
+        <i key={index} style={{ color: "#FFD700" }} className="ri-star-fill" />
+      ))}
+    <p style={{ fontSize: "20px" }}>{feedBack.content}</p>
+  </div>
+);
+
+export default TestimonialItem;
