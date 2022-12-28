8852e150d8e3fa542ad74bec92d474bd390a3261
Piyush Garg <piyushgarg.dev@gmail.com>
2022-12-28 17:51:31 +0530

    Added Cache Control For Images

--- next.config.js
+++ next.config.js
@@ -1,5 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/:all*(svg|jpg|png|webp|jpeg)",
+        locale: false,
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=864000, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
   reactStrictMode: true,
   images: {
     domains: ["i.ytimg.com", "cdn.hashnode.com", "wsrv.nl"],
