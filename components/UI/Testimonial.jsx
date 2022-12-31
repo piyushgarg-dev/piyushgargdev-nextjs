41b257e39254cfc4fc79003761b1354fe39034e2
Piyush Garg <piyushgarg.dev@gmail.com>
2022-12-31 18:35:29 +0530

    auto slide

--- components/UI/Testimonial.jsx
+++ components/UI/Testimonial.jsx
@@ -8,8 +8,9 @@ const Testimonial = ({ feedbacks = [] }) => {
   const settings = {
     dots: false,
     autoplay: true,
-    speed: 1000,
-    autoplaySpeed: 5000,
+    speed: 3500,
+    autoplaySpeed: 0,
+    cssEase: "linear",
     infinite: true,
     swipeToSlide: true,
     slidesToShow: 3,
