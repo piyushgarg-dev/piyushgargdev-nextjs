f9bc8e61b852684897573d3f93f55d64424e5076
mohit <mohitchandola970@gmail.com>
2023-06-07 18:15:14 +0530

    font size changed

--- components/UI/Blog.jsx
+++ components/UI/Blog.jsx
@@ -16,7 +16,7 @@ const Blog = ({ blogs, blogDomain }) => {
         <Row>
           <Col lg="6" md="6" className="mb-5">
             <SectionSubtitle subtitle="blog.piyushgarg.dev" />
-            <h4 className="mt-4 text-[5px]">Checkout my recent blogs</h4>
+            <h4 className="mt-4 text-2xl">Checkout my recent blogs</h4>
           </Col>
         </Row>
 
