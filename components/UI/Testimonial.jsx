93d0f60d65d9833486b83f0626f36c0d3267d2e4
harsh11-11 <h7jain@gmail.com>
2023-06-10 15:48:54 +0530

    fetching last hours changes in OG repo

--- components/UI/Testimonial.jsx
+++ components/UI/Testimonial.jsx
@@ -40,7 +40,7 @@ const Testimonial = ({ feedbacks = [] }) => {
     <section>
       <Container>
         <SectionSubtitle subtitle="Testimonials" />
-        <h4 className="mt-4 mb-5 text-2xl">Feedacks from students</h4>
+        <h4 className="mt-4 mb-5 text-2xl">Feedback from students</h4>
         <Row>
           <Slider {...settings}>
             {feedbacks.map((feedBack) => (
