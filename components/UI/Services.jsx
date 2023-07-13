1eac140f152ac1cc6d65aba20601bf446cb5f750
mahmads0 <139263516+mahmads0@users.noreply.github.com>
2023-07-13 19:25:57 +0500

    Update Services.jsx

--- components/UI/Services.jsx
+++ components/UI/Services.jsx
@@ -38,7 +38,7 @@ const Services = ({ youtubeStats, youtubeVideos }) => {
                         "_blank"
                       )
                     }
-                    style={{ padding: "10px" }}
+                    style={{ padding: "10px"; border-radius:"20px" }}
                     key={video.id.videoId}
                   >
                     <Image
