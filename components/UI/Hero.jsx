225e2862137f8572aa8fa4efddfa6b06fb007507
SurajRaika <103476230+SurajRaika@users.noreply.github.com>
2023-08-21 17:07:39 +0000

    Add Discord icon to Join Discord button

--- components/UI/Hero.jsx
+++ components/UI/Hero.jsx
@@ -5,13 +5,15 @@ import { Container, Row, Col, Button } from "reactstrap";
 import Image from "next/image";
 import heroImg from "../../public/images/PiyushGarg.png";
 import classes from "../../styles/hero.module.css";
-import classNames  from "../../styles/subtitle.module.css";
+import classNames from "../../styles/subtitle.module.css";
+import { RiDiscordFill } from "react-icons/ri";
+
 
 const Hero = () => {
   return (
     <section className={` ${classes.hero} `}>
 
-    {/* desktop / large screen view */}
+      {/* desktop / large screen view */}
       <Container id="hero-section " className="md:flex hidden">
         <Row>
           <Col lg="6" md="6">
@@ -32,11 +34,15 @@ const Hero = () => {
                 <Link
                   target="_blank"
                   href="https://discord.gg/kRSRxBQ6xf"
-                  className="relative text-sm sm:text-md md:text-lg text-center items-center justify-center px-8 py-4  font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                  className="relative text-sm sm:text-md md:text-lg flex items-center justify-center px-8 py-4 font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                   role="button"
                 >
-                  <span className="block">Join Discord Server 🎉</span>
+                  <span className="block">Join Discord Server</span>
+                  <span className="block ml-2">
+                    <RiDiscordFill  size="1.5em"  /> {/* Discord Icon */}
+                  </span>
                 </Link>
+
               </div>
             </div>
           </Col>
@@ -55,21 +61,21 @@ const Hero = () => {
           <Col lg="6" md="6">
             <div className={`${classes.hero__content}`}>
               <div className="flex flex-col items-center justify-center">
-                
+
                 <Col lg="6" md="6">
                   <div className={`${classes.hero__img} text-end`}>
                     <Image alt="Piyush Garg" src={heroImg} width="450" height="450" />
                   </div>
                 </Col>
                 <h2 className="mt-3 mb-3">
-                <span  className={`${classNames.section__subtitle}`} >Hello ! </span>
-                I&apos;m Piyush Garg</h2>
+                  <span className={`${classNames.section__subtitle}`} >Hello ! </span>
+                  I&apos;m Piyush Garg</h2>
                 <h5 className="mb-4">Fullstack Developer & Instructor</h5>
                 <p id="about-me" className="text-center">
-                Hi there! My name is Piyush Garg and I&rsquo;m a software
-                engineer with over 5 years of experience in the industry. I love
-                all things tech and coding, and on my channel, I share my
-                knowledge and experience with others.
+                  Hi there! My name is Piyush Garg and I&rsquo;m a software
+                  engineer with over 5 years of experience in the industry. I love
+                  all things tech and coding, and on my channel, I share my
+                  knowledge and experience with others.
                 </p>
               </div>
 
@@ -80,10 +86,13 @@ const Hero = () => {
                 <Link
                   target="_blank"
                   href="https://discord.gg/kRSRxBQ6xf"
-                  className="relative w-full text-sm sm:text-md md:text-lg text-center items-center justify-center px-8 py-4  font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                  className="relative w-full text-sm sm:text-md md:text-lg flex text-center items-center justify-center px-8 py-4  font-bold text-white transition-all duration-200 bg-gray-900 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                   role="button"
                 >
-                  <span className="block">Join Discord Server 🎉</span>
+                  <span className="block">Join Discord Server</span>
+                  <span className="block ml-2">
+                    <RiDiscordFill  size="1.5em"  /> {/* Discord Icon */}
+                  </span>
                 </Link>
               </div>
             </div>
