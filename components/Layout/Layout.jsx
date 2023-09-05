d5a80d1784bf7b4441a0ac4cfe6f27c08dfeda01
Sunny Kumar <sunnykumar23232@gmail.com>
2023-09-05 22:11:58 +0530

    feat: implement up arrow for goes up

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,13 +1,14 @@
 import React, { Fragment } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-
+import ScrollUp from "../Features/ScrollUp/ScrollUp";
 const Layout = (props) => {
   return (
     <Fragment>
       <Header />
       <div>{props.children}</div>
       <Footer />
+      <ScrollUp />
     </Fragment>
   );
 };
