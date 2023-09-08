56864a61d0386f179bf0c27706b6f9e09476f634
xradeel <xradeel7@gmail.com>
2023-09-08 21:14:34 +0500

    fixing the issue no.#604

--- components/UI/ServicesItem.jsx
+++ components/UI/ServicesItem.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import classes from "../../styles/services-item.module.css";
 
-const ServicesItem = ({ title, icon }) => {
+const ServicesItem = ({ title, icon, index }) => {
   return (
-    <div className={`${classes.service__item}`}>
+    <div
+      className={`${classes.service__item} ${
+        index === 2 ? classes.service__item__second : ""
+      }`}
+    >
       <span>
         <i className={icon}></i>
       </span>
