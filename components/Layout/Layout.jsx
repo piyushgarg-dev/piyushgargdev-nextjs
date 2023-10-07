948d2e34376fd262efecb80b11215f2b857c661f
Parth Movaliya <parthmovaliya55055@gmail.com>
2023-10-07 21:07:53 +0530

    change 'Up' to react-icon

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import React, { useState, Fragment, useEffect } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
-
+import { BsFillArrowUpCircleFill } from "react-icons/bs"
 const Layout = (props) => {
   const [displayHeight, setDisplayHeight] = useState(null);
   const [scrollY, setScrollY] = useState(0);
@@ -26,8 +26,11 @@ const Layout = (props) => {
     <Fragment>
       <Header />
       {displayHeight && ((scrollY) > (displayHeight * (3 / 4))) &&
-        <a href="#" className="text-white fixed right-4 bottom-4 p-2 text-xl border border-white rounded-full bg-white/20 hover:scale-105 duration-300 hover:right-5 hover:bottom-5 cursor-pointer">
-          Up
+        // <a href="#" className="fixed right-4 bottom-4 cursor-pointer text-white scale-150">
+        //   <BsFillArrowUpCircleFill />
+        // </a>}
+        <a href="#" className="z-10 text-white fixed right-4 bottom-4 p-2 text-xl border border-white rounded-full bg-white/20  scale-110 hover:scale-125 duration-300 hover:right-5 hover:bottom-5 cursor-pointer">
+          <BsFillArrowUpCircleFill />
         </a>}
       <div>{props.children}</div>
       <Footer />
