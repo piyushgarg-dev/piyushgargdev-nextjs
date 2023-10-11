1b38b9a907e03d5488a945a248d2e780febe50b8
pratikshelar546 <66733074+pratikshelar546@users.noreply.github.com>
2023-10-11 21:11:09 +0530

    change the color oh header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -111,14 +111,14 @@ const Header = () => {
               {NAV__LINK.map((item, index) => (
                 <div
                   key={index}
-                  className={`${classes.mobile__menuDiv} cursor-pointer`}
+                  className={`${classes.mobile__menuDiv} cursor-pointer `}
                 >
                   <Link aria-label={item.display} href={item.path} target={`${item.openInNewPage?'_blank':'_self'}`}>
                     <p className={`${classes.mobile__menu}`}>{icons[index]}</p>
                   </Link>
 
                   <Link aria-label={item.display} href={item.path} target={`${item.openInNewPage?'_blank':'_self'}`}>
-                    <span className=" text-[#808dad] hover:text-green-400">
+                    <span className=" text-[#cddcdc] hover:text-green-400">
                       {item.display}
                     </span>
                   </Link>
@@ -135,7 +135,7 @@ const Header = () => {
                   </Link>
 
                   <Link href={"/#"}>
-                    <span className=" text-[#808dad] hover:text-green-400">
+                    <span className=" text-[#cddcdc] hover:text-green-400">
                       Sign Out
                     </span>
                   </Link>
@@ -150,7 +150,7 @@ const Header = () => {
                   </Link>
 
                   <Link href={"#"}>
-                    <span className=" text-[#808dad] hover:text-green-400">
+                    <span className=" text-[#cddcdc] hover:text-green-400">
                       Login
                     </span>
                   </Link>
