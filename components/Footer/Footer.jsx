5e9710e976ad628a59b7bd813749a658473edfe0
akhilrocz <b.akhil667@gmail.com>
2023-10-20 19:06:08 +0530

    commit Clear-Title

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -10,12 +10,19 @@ const Footer = () => {
 
   return (
     <footer>
+      <div class="animated">
+        <div class="wave" id="wave1"></div>
+        <div class="wave" id="wave2"></div>
+        <div class="wave" id="wave3"></div>
+        <div class="wave" id="wave4"></div>
+        <div class="wave" id="wave5"></div>
+      </div>
       <Container>
         <Row>
           <Col lg="12">
             <div className={`${classes.footer__copyright}`}>
-            <p>
-                &copy; 2023 Piyush Garg. All rights reserved.
+            <p class="footer-copyright">
+                &copy; 2023 Piyush Garg. All rights reserved.               
             </p>
             </div>
           </Col>
