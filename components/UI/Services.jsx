97b837d5c39f87663f088a2fed5cda689c243ae8
wp120 <parthbhosle2002@gmail.com>
2023-12-11 00:00:24 +0530

    Adds hovering effect to subscribe btn.

--- components/UI/Services.jsx
+++ components/UI/Services.jsx
@@ -89,7 +89,7 @@ const Services = ({ youtubeStats, youtubeVideos }) => {
               target="_blank"
               rel="noreferrer"
             >
-              <Button color="danger">Subscribe</Button>
+              <Button className={`${classes.subscribe_btn}`} color="danger">Subscribe</Button>
             </a>
           </Col>
         </Row>
