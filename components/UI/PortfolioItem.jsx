d48ba2cd5189442972572cda6cae377c97a2a382
Ayush Tripathi <145206910+ayushtripathi15567@users.noreply.github.com>
2024-01-09 00:07:57 +0530

    Revert "fix: my gears page top card height"

--- components/UI/PortfolioItem.jsx
+++ components/UI/PortfolioItem.jsx
@@ -31,7 +31,7 @@ const PortfolioItem = (props) => {
 
           <div className='bg-transparent'>
             <div className={`${classes.portfolio__img}`}>
-              <Image alt={title} src={img} width={380} height={1} style={{maxHeight: "380px", overflow:"auto"}}/>
+              <Image alt={title} src={img} width={380} height={1} />
 
             </div>
 
