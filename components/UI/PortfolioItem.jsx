f2d4ce5988467bfecae66de08bbc27bdaed943ff
officeneerajsaini <officeneerajsaini@gmail.com>
2024-01-22 14:55:14 +0530

    fixes  #1266 text overlapping issue in course card

--- components/UI/PortfolioItem.jsx
+++ components/UI/PortfolioItem.jsx
@@ -38,7 +38,7 @@ const PortfolioItem = (props) => {
             <h3 style={{ background: "transparent" }}>{title}</h3>
             <p style={{ background: "transparent", }}>{subtitle}</p>
             
-            <div className=" w-[100%] mt-5 lg:mt-0"> </div>
+            <div className=" w-[100%] lg:mt-0" style="margin-top: 6rem!important;"> </div>
             <div
               style={{
                 position: "absolute",
